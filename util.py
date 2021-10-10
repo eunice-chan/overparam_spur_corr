c8fdefcd4b2494d2942dafc6f126cb93c679c7a5
Eunice Chan <ekchan@berkeley.edu>
2021-10-10 16:27:40 -0700

    Update util debug statements

--- util.py
+++ util.py
@@ -57,7 +57,8 @@ def robust_acc(output, target, group):
         _, pred = output.topk(1, 1, True, True)
         pred = pred.t()
         correct = pred.eq(target.view(1, -1).expand_as(pred))[:1].view(-1)
-        
+
+        pred=pred.view(1, -1)
         print("Batch size", batch_size)
         print("Group | Target | Pred | Correct")
         for groupi, targeti, predi, correcti in zip(group, target, pred, correct):
