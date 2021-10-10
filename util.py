2eb8b7c7bb0f6d8fb6958419cc0dad02ae68ae6f
Eunice Chan <ekchan@berkeley.edu>
2021-10-10 16:32:43 -0700

    Update util debug statements

--- util.py
+++ util.py
@@ -58,7 +58,8 @@ def robust_acc(output, target, group):
         pred = pred.t()
         correct = pred.eq(target.view(1, -1).expand_as(pred))[:1].view(-1)
 
-        pred=pred.numpy()
+        print(pred.get(0))
+        pred=pred.cpu().numpy()
         print(pred)
         print("Batch size", batch_size)
         print("Group | Target | Pred | Correct")
